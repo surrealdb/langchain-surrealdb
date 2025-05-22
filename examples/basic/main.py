b7adfdb99f0af014bcae1919d09d0d53a14974d7
Martin Schaer <martin.schaer@surrealdb.com>
2025-05-22 14:03:32 +0100

    update notebook

--- examples/basic/main.py
+++ examples/basic/main.py
@@ -9,13 +9,17 @@ conn.signin({"username": "root", "password": "root"})
 conn.use("langchain", "demo")
 vector_store = SurrealDBVectorStore(OllamaEmbeddings(model="llama3.2"), conn)
 
-doc_1 = Document(page_content="foo", metadata={"source": "https://surrealdb.com"})
-doc_2 = Document(page_content="SurrealDB", metadata={"source": "https://surrealdb.com"})
+_url = "https://surrealdb.com"
+d1 = Document(page_content="foo", metadata={"source": _url})
+d2 = Document(page_content="SurrealDB", metadata={"source": _url})
+d3 = Document(page_content="bar", metadata={"source": "https://example.com"})
+d4 = Document(page_content="this is surreal", metadata={"source": _url})
 
-vector_store.add_documents(documents=[doc_1, doc_2], ids=["1", "2"])
+vector_store.add_documents(documents=[d1, d2, d3, d4], ids=["1", "2", "3", "4"])
 
-results = vector_store.similarity_search_with_score(
-    query="surreal", k=1, custom_filter={"source": "https://surrealdb.com"}
-)
+q = "surreal"
+filter = {"source": _url}
+print(f'\nSearch: "{q}" filter {filter}\n')  # noqa: T201
+results = vector_store.similarity_search_with_score(query=q, k=2, custom_filter=filter)
 for doc, score in results:
-    print(f"* [SIM={score:3f}] {doc.page_content} [{doc.metadata}]")  # noqa: T201
+    print(f"[similarity={score:.0%}] {doc.page_content}")  # noqa: T201
